07e6686481a89dfed1ee77df5aa794682d9f69ba
Ivan Rychkov <ivan.rychkov.dev@gmail.com>
2021-02-16 21:23:09 +0500

    Add index controller

---
+++ src/Controller/Rest/v1/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Rest\v1;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(condition="request.attributes.get('version') == 'v1'")
+ */
+final class IndexController extends AbstractFOSRestController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            'code' => JsonResponse::HTTP_OK,
+            'message' => '',
+        ]);
+    }
+}
