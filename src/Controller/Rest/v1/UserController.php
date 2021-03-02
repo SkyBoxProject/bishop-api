9608e2e8cb6aa9f00faf0eec7b2e42265f770fff
Ivan-inc <i.pirog.gg@gmail.com>
2021-03-02 22:24:13 +0500

    Добавлен эндпоинт на получение данных текущего пользователя

---
+++ src/Controller/Rest/v1/UserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Rest\v1;
+
+use App\Domain\User\Entity\User;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Nelmio\ApiDocBundle\Annotation\Operation;
+use OpenApi\Annotations as OA;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @Route(condition="request.attributes.get('version') == 'v1'")
+ */
+final class UserController extends AbstractFOSRestController
+{
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @Operation(
+     *     tags={"Пользователь"},
+     *     summary="Получить данные текущего пользователя",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Возвращается, когда успех",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="code", example=200, type="integer"),
+     *             @OA\Property(property="message", type="string"),
+     *             @OA\Property(property="response", type="object",
+     *                 @OA\Property(property="id", type="string"),
+     *                 @OA\Property(property="email", type="string"),
+     *                 @OA\Property(property="roles", type="array", @OA\Items(type="string"))
+     *             )
+     *         )
+     *     )
+     * )
+     *
+     * @Route("/user", methods={"GET"})
+     */
+    public function getUserInfo(Request $request): JsonResponse
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return new JsonResponse([
+            'code' => JsonResponse::HTTP_OK,
+            'message' => $this->translator->trans('Success', [], 'message'),
+            'response' => [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+            ],
+        ]);
+    }
+}
--- src/Domain/Security/UserRole.php
+++ src/Domain/Security/UserRole.php
@@ -7,5 +7,4 @@ final class UserRole
     public const ROLE_USER = 'ROLE_USER';
     public const ROLE_VERIFIED = 'ROLE_VERIFIED';
     public const ROLE_ADMIN = 'ROLE_ADMIN';
-    public const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
 }
